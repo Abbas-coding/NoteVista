d753ff6269790e8683aac43c7a30d91186b6a87a
Abbas-coding <abbasfakhruddin205@gmail.com>
2023-10-09 20:30:17 +0500

    Express Server and Mongoose Setup

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI)
+    console.log("connected")
+}
+
+module.exports = connectToMongo
