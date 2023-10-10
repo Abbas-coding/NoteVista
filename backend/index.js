3faa45a40e430e1307eebea7a8dcf2b74b6a9a8b
Abbas-coding <abbasfakhruddin205@gmail.com>
2023-10-10 08:02:22 +0500

    Creating Routes & Schema for Storing Notes & Users

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, I am Abbas learning about mongoose!')
-})
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
