5228ef1932e8bf9fab914a93cd3374340b0b2039
Abbas-coding <abbasfakhruddin205@gmail.com>
2023-10-11 10:51:46 +0500

    Context Api and useLocation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
+    <NoteState>
     <Router>
       <Navbar />
+      <div className='container'>
       <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/about" element={<About />} />
       </Routes>
+      </div>
     </Router>
+    </NoteState>
     </>
   );
 }
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NoteContext from "./NoteContext";
+
+const NoteState = (props)=>{
+    return(
+        <NoteContext.Provider value={{}} >
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
